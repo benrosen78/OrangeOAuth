9cf6d9d0d7d0320cc24927c0210babf6c1e7a0e2
Ben Rosen <benrosen78@gmail.com>
2015-06-13 13:42:35 -0500

    clean up proj

--- Example/OrangeOAuth Example/ORViewController.m
+++ Example/OrangeOAuth Example/ORViewController.m
@@ -27,20 +27,25 @@
 }
 
 - (IBAction)buttonPressed:(UIButton *)sender {
+    
     // [CARedditOAuth sharedInstance] is the instance you should use.
     CARedditOAuth *redditOAuth = [CARedditOAuth sharedInstance];
+    
     // Client identifier is from the Reddit OAuth API.
     redditOAuth.clientIdentifier = @"RV9IIn94H5YmNQ";
+    
     // Scopes are the permissions you want on this users Reddit account.
     redditOAuth.scopes = @[@"identity"];
+    
     // This is the URL scheme you created earlier.
     redditOAuth.urlScheme = @"com.contextdev.orangeoauth";
+    
     // Set the delegate so that the library can work.
     redditOAuth.delegate = self;
+    
     // This will start the OAuth process.
     [redditOAuth startRedditOAuth];
-    /*
-     */
+    
 }
 
 - (void)redditOAuth:(CARedditOAuth *)redditOAuth presentViewController:(UIViewController *)viewController {
